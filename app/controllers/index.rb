f92948740874e44e35231dcedc2f192d63dd51f1
Your Name <you@example.com>
2016-07-28 20:24:18 -0500

    Merge conflict

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,16 +3,12 @@ get '/' do
 end
 
 post '/' do
-  # sentence = "Jedi+must+use+the+force"
-  # request_url = URI.parse("https://yoda.p.mashape.com/yoda?sentence=#{sentence}")
   sentence = params[:original_text]
-  # p sentence
   response = Unirest.get "https://yoda.p.mashape.com/yoda?sentence=#{sentence}",
   headers:{
     "X-Mashape-Key" => "G16eP2ZCXXmshMEuYPyGdGEg5pD6p15pMOGjsn4iSu7CJTrhed",
     "Accept" => "text/plain"
   }
   response.body
-  # p @printable_response
   # erb :index
 end
